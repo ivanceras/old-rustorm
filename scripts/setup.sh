671ac468eb8e19d3d3fe39ca5c320f2c7172a330
ivan ceras <ivanceras@gmail.com>
2015-07-04 22:41:45 +0800

    remove password prompt on test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ## create a new database, will prompt for password
-psql -U postgres -W -h localhost -p 5432 -d postgres -c "create database bazaar_v6 with owner postgres encoding 'utf8';"
+psql -U postgres -h localhost -p 5432 -d postgres -c "create database bazaar_v6 with owner postgres encoding 'utf8';"
 
 ## fill the newly created database, will prompt password again
-psql -U postgres -W -h localhost -p 5432 -d bazaar_v6 -f bazaar_v6_all.sql
+psql -U postgres -h localhost -p 5432 -d bazaar_v6 -f bazaar_v6_all.sql
+
+psql -U postgres -h localhost -p 5432 -d postgres -c "alter role postgres set password 'p0stgr3s'"
