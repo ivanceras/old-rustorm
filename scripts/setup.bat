cd08188d5531bd6e1770594832340f2d55c2d29a
ivanceras <ivanceras@gmail.com>
2015-11-11 04:59:54 +0800

    attempting for postgresql setup for windows in appveyor

---
+++ scripts/setup.bat
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## create a new database, will prompt for password
+psql.exe -U postgres -h localhost -p 5432 -d postgres -c "create database bazaar_v6 with owner postgres encoding 'utf8';"
+
+## create role bazaar
+psql.exe -U postgres -h localhost -p 5432 -d postgres -c "create role bazaar with login password 'b4z44r'"
+
+
+## fill the newly created database, will prompt password again
+psql.exe -U postgres -h localhost -p 5432 -d bazaar_v6 -f .\scripts\bazaar_v6_all.sql
+
+
+## change the password of postgresql as what is used in the examples and tests
+psql.exe -U postgres -h localhost -p 5432 -d postgres -c "alter role postgres with password 'p0stgr3s'"
