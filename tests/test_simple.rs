eebd6ca9f3f3105b6fd3186911a762cd4e30992d
Jovansonlee Cesar <ivanceras@gmail.com>
2017-02-15 02:28:13 +0800

    added test, rethink on query builder

---
+++ tests/test_simple.rs
@@ -0,0 +1,65 @@
+extern crate rustorm;
+
+use rustorm::query::Query;
+use rustorm::query::Filter;
+use rustorm::query::Select;
+use rustorm::query::Equality::EQ;
+use rustorm::database::BuildMode;
+use rustorm::pool::Platform;
+use rustorm::platform::Postgres;
+
+#[test]
+#[cfg(feature = "postgres")]
+fn test_pg(){
+    let pg = Platform::pg();
+    let mut query = Select::new();
+    query.from(&"users".to_string());
+    let filter = Filter::new("username", EQ, &"Hello".to_string());
+    query.add_filter(filter);   
+    let sql = pg.build_select(&query, &BuildMode::Debug); 
+    println!("{}", sql);
+    let expected = r#"
+   SELECT 
+     FROM users
+    WHERE username = 'Hello'
+    "#;
+    assert_eq!(sql.sql.trim(), expected.trim());
+}
+
+#[test]
+#[cfg(feature = "sqlite")]
+fn test_sqlite(){
+    let pg = Platform::sqlite();
+    let mut query = Select::new();
+    query.from(&"users".to_string());
+    let filter = Filter::new("username", EQ, &"Hello".to_string());
+    query.add_filter(filter);   
+    let sql = pg.build_select(&query, &BuildMode::Debug); 
+    println!("{}", sql);
+    let expected = r#"
+   SELECT 
+     FROM users
+    WHERE username = 'Hello'
+    "#;
+    assert_eq!(sql.sql.trim(), expected.trim());
+}
+
+
+#[test]
+#[cfg(feature = "mysql")]
+fn test_mysql(){
+    let pg = Platform::mysql();
+    let mut query = Select::new();
+    query.from(&"users".to_string());
+    let filter = Filter::new("username", EQ, &"Hello".to_string());
+    query.add_filter(filter);   
+    let sql = pg.build_select(&query, &BuildMode::Debug); 
+    println!("{}", sql);
+    let expected = r#"
+   SELECT 
+     FROM users
+    WHERE username = 'Hello'
+    "#;
+    assert_eq!(sql.sql.trim(), expected.trim());
+}
+
